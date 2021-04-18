002d4e5ba5d7e2dc01e0791417384b3c476b8080
YFZeng07 <yz643@duke.edu>
2021-04-18 15:12:36 -0400

    Update data wrangling

--- Data wrangling.R
+++ Data wrangling.R
@@ -2,3 +2,15 @@
 library(tidyverse)
 
 #import data
+FallsLake <- read.csv("./Data/Raw/02087183_FallsLake_daily.csv") %>%
+  select(2, 3, 6) %>%
+  mutate(gage = "Falls Lake",
+         datetime = as.Date(datetime, format = "%m/%d/%Y")) %>%
+  rename(discharge_mean = Discharge..cubic.feet.per.second..Minimum..1)
+
+
+Clayton <- read.csv("./Data/Raw/02087500_Clayton_daily.csv") #%>%
+  select(2, 3, 6) %>%
+  mutate(gage = "Clayton",
+         datetime = as.Date(datetime, format = "%m/%d/%Y")) %>%
+  rename(discharge_mean = Discharge..cubic.feet.per.second..Minimum..1)
